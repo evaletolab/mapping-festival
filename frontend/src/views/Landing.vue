b3471ec95e52cf7572430e9ba04168f9b196f9ba
olivier <olivier@evaletolab.ch>
2021-04-08 19:24:07 +0200

    chg: add missing footer

--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -7,17 +7,29 @@
       <div class="title2">{{t(config.landing.title2)}}</div>
       <div class="title3">{{t(config.landing.title3)}}</div>
     </section>
+    <!-- MENU LINKS -->
     <ul class="links">
       <li v-for="menu in config.landing.menu" :key="menu.link">
         <router-link :to="menu.link">{{ t(menu.name) }}</router-link>
       </li>
     </ul>
 
+    <!-- FOR TEST -->
     <ul>
       <li v-for="event in events" :key="event._id">
         {{ t(event.title) }} 
       </li>
     </ul>
+
+    <!-- FOOTER LINKS -->
+    <div class="footer">
+      <ul>
+        <li v-for="menu in config.footer.menu" :key="menu.link">
+          <router-link :to="menu.link">{{ t(menu.name) }}</router-link>
+        </li>
+      </ul>
+    </div>
+
 
   </div>
 </template>
@@ -35,7 +47,28 @@
     }
 
     ul.links{
+      padding: 0;
       list-style: none;
+      li{
+        padding: 10px ;
+      }
+    }
+
+    .footer{
+      position: fixed;
+      bottom: 0px;
+      left: 0;
+      right: 0;
+      ul{
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        li{
+          float: left;
+          padding: 2px 10px;
+          text-decoration: none;
+        }
+      }
     }
   }
 
