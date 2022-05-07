0139b0cce04eb4749234faf5282ceef3ccceaa6a
david hodgetts <david.demainlalune@gmail.com>
2022-05-07 16:53:16 +0200

    removes description on HeaderInfo

--- frontend/src/components/HeaderInfo.vue
+++ frontend/src/components/HeaderInfo.vue
@@ -5,17 +5,15 @@
       config.landing.title2 : Visual audio…
       config.landing.title3 : Geneva
       -->
-        <div class="description">
+        <!-- <div v-if="isMobileView" class="description">
           <h2 class="white" v-html="config.landing.title1"/> 
           <div class="white">Visual audio and<br>deviant electronics</div>
           <div class="margin-top1 white">19 &#8239; — &#8239; 29.5.2022</div>
           <div class="white">Geneva</div>
-        </div> 
-          <img class="cover" src="/img/mapping_header3.png" alt="">
+        </div>  -->
         
+        <img class="cover" src="/img/mapping_header3.png" alt="">
       </div>
-
-
 </template>
 
 <style scoped>
@@ -31,6 +29,7 @@
     }
     .description{
         position:absolute;
+        background-color: rgba(0, 0, 0, 0.7);
         top: 10px;
         left: 10px;
     }
@@ -50,12 +49,29 @@ import { $config } from '../services';
 export default class HeaderInfo extends Vue {
 
 
+  screenWidth = 0;
+
+  get isMobileView(){
+    return this.screenWidth <= 476;
+  }
+
   mounted(){
+    window.addEventListener('resize', this.onResize);
+    this.computeScreenWidth();
   }
 
-  beforeDestroy(){
-
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize);
   }
+  
+  computeScreenWidth(){
+    this.screenWidth = window.innerWidth;
+  }
+
+  onResize(){
+    this.computeScreenWidth();
+  }
+
   get config(){
     return $config.store.config;
   }
