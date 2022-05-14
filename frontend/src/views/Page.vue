a1b401c400fe66cf7d825a199bc2466872a7093e
ddelcourt <ddelcourt@graphistes.ch>
2022-05-15 00:55:10 +0200

    Trying a header fix on page.vue

--- frontend/src/views/Page.vue
+++ frontend/src/views/Page.vue
@@ -3,55 +3,16 @@
   <!-- TESTING READ-ONLY DISPLAY -->
   <!--         ---------         -->    
   <div class="page ">
-    <!-- <div class="header hide-sm"> -->
+    <div class="header hide-sm">
 
     <!-- ************************ -->
     <!-- ******** Header ******** -->
     <!-- ************************ -->
 
     <div class="header-wrapper hide-sm">
+      <header-info />
 
-      <div class="header-info">
-      <!-- 
-      config.landing.title1 : Mapping festival
-      config.landing.title2 : Visual audio…
-      config.landing.title3 : Geneva
-      -->
-      
-      <!------------->
-      <!-- Title 1 -->
-      <!------------->
-        <h2 class="white" v-html="config.landing.title1"/> 
-        <!-- <h1 class="" v-html="t(config.landing.title1)"/> -->
-      
-      <!------------->
-      <!-- Title 2 -->
-      <!------------->
-        <!-- <div  v-for="(title,index) in t(config.landing.title2).split('\n')" :key="index">
-          {{title}}</div> -->
-        <div class="white">Visual audio and<br>deviant electronics</div>
-
-      <!------------->
-      <!--- Dates --->
-      <!------------->
-        <div class="margin-top1 white">19 &#8239; — &#8239; 29.5.2022</div>
-      
-      <!------------->
-      <!-- Title 3 -->
-      <!------------->
-      
-        <!--  <div  v-html="t(config.landing.title3)"  class="hide-sm"/> -->
-        <div  class="white" v-for="(title,index) in t(config.landing.title3).split('\n')" :key="index">
-        {{title}}</div>
-        
-      </div>
-
-      <!---------------------->
-      <!-- Background image -->
-      <!---------------------->
-
-      <div class="header-image"></div>
-
+    </div>
 
     <!-- ************************ -->
     <!-- ******* /Header ******** -->
