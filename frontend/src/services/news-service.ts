f96628c231f99637a931e87ed6a191afa9e8bafa
david hodgetts <david.demainlalune@gmail.com>
2022-05-15 13:37:15 +0200

    fixes incorrect merge on news-service.ts

--- frontend/src/services/news-service.ts
+++ frontend/src/services/news-service.ts
@@ -4,29 +4,17 @@ import { CMS } from "@/models";
 class NewsService
 {
     get all(): CMS.News[]{
-<<<<<<< .merge_file_SNg91l
-        return $cms.news
-        .sort((a, b) => b.created.getTime() - a.created.getTime());
-    }
-
-    get pinnedItems(): CMS.News[]{
-=======
         return $cms.news;
     }
 
     // pinned status is obsolete
     private get pinnedItems(): CMS.News[]{
->>>>>>> .merge_file_8lcia4
         return this.all.filter(item => item.highlighted)
         .sort((a, b) => b.created.getTime() - a.created.getTime());
     }
 
-<<<<<<< .merge_file_SNg91l
-    get unpinnedItems(): CMS.News[]{
-=======
     // pinned status is obsolete
     private get unpinnedItems(): CMS.News[]{
->>>>>>> .merge_file_8lcia4
         return this.all.filter(item => !item.highlighted)
         .sort((a, b) => b.created.getTime() - a.created.getTime());
     }
@@ -35,17 +23,7 @@ class NewsService
         if(count <= 0){
             return [];
         }
-<<<<<<< .merge_file_SNg91l
-
-        let result = this.pinnedItems.slice(0, count);
-        const remainder = count - result.length;
-        if(remainder > 0){
-            const remainingItems = this.unpinnedItems.slice(0, remainder);
-            result = [...result, ...remainingItems];
-        }
-=======
         let result = this.all.slice(0, count);
->>>>>>> .merge_file_8lcia4
 
         return result;
     }
