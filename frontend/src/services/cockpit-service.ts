5ef058ca6cc1afcccd9679c603e2ab653fd1866b
david hodgetts <david.demainlalune@gmail.com>
2022-09-07 23:15:01 +0200

    fixes calendar bug related to daylight savings

--- frontend/src/services/cockpit-service.ts
+++ frontend/src/services/cockpit-service.ts
@@ -238,8 +238,8 @@ class CockpitService {
         const v = w.value;
         // new Date("2021-05-06T16:25:00+02:00");
 
-        const start = new Date(`${v.startDate}T${v.startHour}:00+02:00`);
-        const end = new Date(`${v.endDate}T${v.endHour}:00+02:00`);
+        const start = new Date(`${v.startDate}T${v.startHour}`);
+        const end = new Date(`${v.endDate}T${v.endHour}`);
         const cancel = v.cancel;
         // console.log(v.startDate, v.startHour, start);
         // console.log(v.endDate, v.endHour, end);
